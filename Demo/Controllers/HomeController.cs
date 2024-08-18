604f107c7b34df7ce553928201617f9582ae3532
Rushikesh gaikwad <59560520+rushi1099@users.noreply.github.com>
2024-08-18 21:18:50 +0530

    Add project files.

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using Demo.Models;
+using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult GetList()
+        //{
+        //    List<MyModel> data = MyModel.GetData();
+        //    return View("Index", data);
+        //}
+
+        [HttpPost]
+        public ActionResult Load()
+        {
+            // Load the data and pass it to the view
+            List<MyModel> books = MyModel.GetData();
+            return View("Index", books);
+        }
+
+        [HttpPost]
+        public ActionResult AddToShoppingCart(int bookId)
+        {
+            // Logic to add the book to the shopping cart
+            // For now, you can just simulate with a TempData message
+            TempData["Message"] = $"Book with ID {bookId} added to the cart.";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Commit()
+        {
+            // Logic to commit the changes
+            TempData["Message"] = "Changes have been committed.";
+            return RedirectToAction("Index");
+        }
+        public void OnPostCommit()
+        {
+            string repoPath = Directory.GetCurrentDirectory(); // Path to your local Git repository
+            string remoteName = "startup"; // The name of the remote repository
+            string branchName = "main"; // The branch you want to push to
+            string username = "RD-FD"; // Your remote repository username
+            string password = "needWork@12"; // Your remote repository password
+
+            try
+            {
+                PushChanges(repoPath, remoteName, branchName, username, password);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void PushChanges(string repoPath, string remoteName, string branchName, string username, string password)
+        {
+            try
+            {
+                // Open the repository
+                using (var repo = new Repository(repoPath))
+                {
+                    // Create credentials for authentication
+                    var credentials = new UsernamePasswordCredentials
+                    {
+                        Username = username,
+                        Password = password
+                    };
+
+                    // Find the remote
+                    var remote = repo.Network.Remotes[remoteName];
+                    if (remote == null)
+                    {
+                        Console.WriteLine($"Remote '{remoteName}' not found.");
+                        return;
+                    }
+
+                    // Create a push options object
+                    var pushOptions = new PushOptions
+                    {
+                        CredentialsProvider = (url, user, cred) => credentials
+                    };
+
+                    // Get the reference to the branch
+                    var branch = repo.Branches[branchName];
+                    if (branch == null)
+                    {
+                        Console.WriteLine($"Branch '{branchName}' not found.");
+                        return;
+                    }
+
+                    // Push the branch to the remote
+                    repo.Network.Push(branch, pushOptions); //repo.Network.Push(branch, pushOptions);
+                    // if (result.Status == PushStatus.UpToDate)
+                    // {
+                    //     Console.WriteLine("Push was successful.");
+                    // }
+                    // else
+                    // {
+                    //     Console.WriteLine("Push was not successful.");
+                    // }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Demo/Models/MyModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class MyModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Pages { get; set; }
+        public string Author { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public int InStock { get; set; }
+
+        // Static method to simulate data fetching
+        public static List<MyModel> GetData()
+        {
+            return new List<MyModel>
+            {
+                //new MyModel { ID = 1, Name = "Book 1", Pages = 300, Author = "Author 1", ImageUrl = "url1.jpg", Price = 29.99, InStock = 10 },
+                //new MyModel { ID = 2, Name = "Book 2", Pages = 250, Author = "Author 2", ImageUrl = "url2.jpg", Price = 19.99, InStock = 5 },
+                //new MyModel { ID = 3, Name = "Book 3", Pages = 400, Author = "Author 3", ImageUrl = "url3.jpg", Price = 39.99, InStock = 2 }
+
+               new MyModel {Name="Rama II", Author="Arthur C. Clark", Pages=281,ImageUrl="rama.jpg", InStock=54, Price=44.23 },
+               new MyModel {Name="Exhalation", Author="Ted Chiang", Pages=556,ImageUrl="exhalation.jpg", InStock=13, Price=50.99 },
+               new MyModel {Name="Traffic Secrets", Author="Russell Brunson", Pages=306,ImageUrl="traffic.jpg", InStock=65, Price=18.97 },
+               new MyModel {Name="Clean Code", Author="Robert Martin", Pages=464,ImageUrl="clean.jpg", InStock=15, Price=87.00 }
+            };
+        }
+    }
+}
